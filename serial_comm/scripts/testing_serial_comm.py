fa2a0f5c8580209bfaa52c99893d61a64c799913
odroid <odroid@odroid-desktop.(none)>
2015-04-22 16:12:50 -0500

    fixes to serial comm test

--- serial_comm/scripts/testing_serial_comm.py
+++ serial_comm/scripts/testing_serial_comm.py
@@ -15,52 +15,56 @@ goal of sending motor commands. Dynamics are used to convert the
 cmd_vel to two 8-bit motor speeds 0-255, where 128 is not moving.
 '''
 
-ser = serial.Serial("/dev/ttyACM0", 9600, bytesize=serial.EIGHTBITS,
+ser = serial.Serial("/dev/ttyACM0", 115200, bytesize=serial.EIGHTBITS,
                      parity = serial.PARITY_NONE,
                      stopbits=serial.STOPBITS_ONE,
-                     timeout=4,
+                     timeout=2,
                      xonxoff=0,
                      rtscts=0)
 print (ser.readline())
 
 def callback(command):
     
+    # convert the velocity commands to right and left tread speeds using
+    # the robot physics
     linear = command.command.cmd_vel.linear.x
     angular = command.command.cmd_vel.angular.z
     left = ((linear + 0.307 * angular) / 2) / 0.32675
     right = ((linear - 0.307 * angular) / 2) / 0.32675
-
+    
+    # convert speeds to 0-255 unisgned ascii char
+    # this might be slightly off still
     left = (left + 1) / 2
     if left > 1: left = 1
     right = (right + 1) / 2
     if right > 1: right = 1
-    right = right
     left = math.floor(left*255)
     right = math.floor(right*255)
-    print("left: ", left)
-    print("right: ", right)
 
-
-
-
-    motors = 0
-    paddle_position = command.command.paddle_position
-    bin_position = command.command.bin_position
-    paddle_status = command.command.paddle_status
+    paddle_position = '1' if command.command.paddle_position else '0'
+    bin_position = '1' if command.command.bin_position else '0'
+    paddle_status = '1' if command.command.paddle_status else '0'
 
     thingtosend = "%c%c%c%c%c" % (int(left),int(right),paddle_position,
                                   bin_position,paddle_status)
-    print(thingtosend)
-    print(len(thingtosend))
-    print("[%d] [%d] [%d] [%d] [%d]" % (ord(thingtosend[0]),
+    print('Sent: [%d] [%d] [%c] [%c] [%c]' % (ord(thingtosend[0]),
                                         ord(thingtosend[1]),
-                                        ord(thingtosend[2]),
-                                        ord(thingtosend[3]),
-                                        ord(thingtosend[4])))
-    if ser.isOpen(): print("serial is open")
+                                        thingtosend[2],
+                                        thingtosend[3],
+                                        thingtosend[4]))
+    if ser.isOpen(): print('serial is open')
     ser.write(thingtosend)
     ser.flushOutput()
-    print(ser.readline())
+
+    print("Bytes in buffer:", ser.inWaiting())
+    ack = ser.read(ser.inWaiting())
+    print(ack)
+    ser.flushInput()
+    print("Ack:  [%d] [%d] [%c] [%c] [%c]" % (ord(ack[0]),
+                                        ord(ack[1]),
+                                        ack[2],
+                                        ack[3],
+                                        ack[4]))
 
 
 def main():
