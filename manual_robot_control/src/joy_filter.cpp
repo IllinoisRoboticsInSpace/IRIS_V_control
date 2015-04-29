ae495738ecf7aae0a31039d6b5098373d02a027e
Gil Shohet <shohet2@illinois.edu>
2015-04-29 17:54:30 -0500

    launch file for control laptop stuff

--- manual_robot_control/src/joy_filter.cpp
+++ manual_robot_control/src/joy_filter.cpp
@@ -10,10 +10,10 @@ inline double pow2(double a) {return a * a;}
 
 // controller deadzones
 // TODO: makes these use parameters
-double x_deadzone = 0.06;
-double z_deadzone = 0.12;
-double change_tol = 0.05;
-double max_stick_rate = 10;
+double x_deadzone;
+double z_deadzone;
+double jitter_tol;
+double max_stick_rate;
 
 // header stuff
 unsigned int seq = 0;
@@ -25,12 +25,12 @@ void callback_joy(const sensor_msgs::Joy::ConstPtr & tmpjoy)
 {
   sensor_msgs::Joy joy = *tmpjoy;
 
+  bool changed = false;
+
   // check deadzone
   if (joy.axes[0] > -z_deadzone && joy.axes[0] < z_deadzone) joy.axes[0] = 0;
   if (joy.axes[1] > -x_deadzone && joy.axes[1] < x_deadzone) joy.axes[1] = 0;
    
-  bool changed = false;
-
   // check if buttons changed
   for (int i = 0; i < 8; i++)
   {
@@ -40,6 +40,12 @@ void callback_joy(const sensor_msgs::Joy::ConstPtr & tmpjoy)
       break;
     }
   }
+
+  if ((joy.axes[0] == 0 && joy.axes[1] == 0) &&
+      (prev.axes[0] != 0 || prev.axes[0] != 0))
+  {
+    changed = true;
+  }
   
   // check if axes changed more than tolerance if enough time has passed
   unsigned int dt = (joy.header.stamp - prev.header.stamp).toNSec() / 1000000;
@@ -47,7 +53,7 @@ void callback_joy(const sensor_msgs::Joy::ConstPtr & tmpjoy)
   {
     for (int i = 0; i < 2; i++)
     {
-      if (pow2(joy.axes[i] - prev.axes[i]) > pow2(change_tol))
+      if (pow2(joy.axes[i] - prev.axes[i]) > pow2(jitter_tol))
       {
         changed = true;
         break;
@@ -73,6 +79,14 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joy_filter");
   ros::NodeHandle n;
+  ros::NodeHandle n_("~");
+
+  // set the parameters
+  n_.param<double>("threshold/x", x_deadzone, 0.10);
+  n_.param<double>("threshold/z", z_deadzone, 0.12);
+  n_.param<double>("threshold/jitter", jitter_tol, 0.05);
+  n_.param<double>("max_rate", max_stick_rate, 10);
+
   pub = n.advertise<sensor_msgs::Joy>("/joy_filtered", 1);
   
   sub_joy = n.subscribe<sensor_msgs::Joy>("/joy", 1, callback_joy);
