1ec448fece4a9494f430ac7adb4c51b6b23f1486
Gil Shohet <shohet2@illinois.edu>
2015-05-06 19:47:55 -0500

    added roboteq communication (usb -> rs232)

--- roboteq_comm.py
+++ roboteq_comm.py
@@ -16,13 +16,12 @@ goal of sending motor commands. Dynamics are used to convert the
 cmd_vel to two 8-bit motor speeds 0-255, where 128 is not moving.
 '''
 
-ser = serial.Serial("/dev/ttyACM0", 115200, bytesize=serial.EIGHTBITS,
+ser = serial.Serial("/dev/ttyUSB0", 115200, bytesize=serial.EIGHTBITS,
                      parity = serial.PARITY_NONE,
                      stopbits=serial.STOPBITS_ONE,
                      timeout=2,
                      xonxoff=0,
                      rtscts=0)
-print (ser.readline())
 
 def callback_command(command):
     
@@ -30,29 +29,29 @@ def callback_command(command):
     # the robot physics
     linear = command.command.cmd_vel.linear.x
     angular = command.command.cmd_vel.angular.z
-    left = ((linear + 0.307 * angular) / 2) / 0.4035
-    right = ((linear - 0.307 * angular) / 2) / 0.4035
+    left = ((linear + 0.307 * angular) / 2) # / 0.4035
+    right = ((linear - 0.307 * angular) / 2) # / 0.4035
     
     # convert speeds to [-1000,1000] integer
     # this might be slightly off still
-    left = int(left * 1000)
-    if left > 1000: left = 1000
-    if left < -1000: left = -1000
-    right = int(-right * 1000)
-    if right > 1000: right = 1000
-    if right < -1000: right = -1000
+    left = int(left * 4000)
+    right = int(-right * 4000)
+    if abs(left) > 1000: 
+        factor = float(abs(left)) / 1000
+        left = int(left / factor)
+        right = int(right / factor)
+    if abs(right) > 1000:
+        factor = float(abs(right)) / 1000
+        left = int(left / factor)
+        right = int(right / factor)
 
-    left = str(left)
-    right = str(right)
+    left = '!G 2 ' + str(left) + '\r'
+    right = '!G 1 ' + str(right) + '\r'
 
-    paddle_position = '1' if command.command.paddle_position else '0'
-    bin_position = '1' if command.command.bin_position else '0'
-    paddle_onoff = '1' if command.command.paddle_status else '0'
-
-    thingtosend = left + ',' + right + ',' + padde_position + ','
-                  + bin_position + ',' + paddle_onoff + '!' 
-    print(thingtosend)
-    ser.write(thingtosend)
+    print('Sending:\n' + left.encode('string_escape') + '\n' 
+          + right.encode('string_escape') + '\n')
+    ser.write(left)
+    ser.write(right)
     ser.flushOutput()
 
 
@@ -60,7 +59,7 @@ def callback_trigger(trigger):
 #   print("Got triggered")
     print('Bytes in buffer: ', ser.inWaiting())
     ack = ser.read(ser.inWaiting())
-    print('Ack: ', ack)
+    print("Ack: " + ack + '\n')
     ser.flushInput()
 
 
